3c8e7cc431fe7850619d20716338de5f3893fd23
stupidfish <2928109164@qq.com>
2021-08-31 22:03:44 +0800

    PriceManager 加入价格信息序列化入文件和读取(没测试呢 应该还不可靠)

--- src/main/kotlin/com/stupidfish/task/PriceManager.kt
+++ src/main/kotlin/com/stupidfish/task/PriceManager.kt
@@ -1,13 +1,50 @@
 package com.stupidfish.task
 
-import com.stupidfish.task.struct.M720status
-import com.stupidfish.task.struct.Phisstatus
+import com.stupidfish.task.struct.PriceInfo
+import org.yaml.snakeyaml.Yaml
+import java.io.File
+import java.io.FileNotFoundException
 
-class PriceManager(coinTypeIN:String) {
+
+class PriceManager(coinTypeIN: String) {
+    /*
+    此类建成后勿随意销毁
+     */
+    var runtimes = 0
     val coinType = coinTypeIN
-    fun getOncePrice(){
+    fun getOncePrice() {
         /*
         查询一次价格信息并返回
          */
+        try {
+
+            if (runtimes == 0) {
+                if (File("$coinType.yml").isFile) {
+                    //如果刚开软件但有上次的记录
+                   // val priceInfoHistory =
+                }
+            }
+        } catch (e: FileNotFoundException) {
+            //如果没有文件 实实在在的第一次运行
+        }
+
+        runtimes++
+    }
+
+    fun writeLogToFile(priceInfo: PriceInfo) {//读取一次价格信息并且把关键信息写入文件
+        try {
+            val f = File("$coinType.yml")
+            f.writeText(Yaml().dump(priceInfo))//写入关键信息
+        } catch (e: FileNotFoundException) {
+            File("$coinType.yml").createNewFile()
+        }
+    }
+
+    fun readLogToFile() {
+        /*
+        从文件里读取上次的价格关键信息
+         */
+        val list: Map<String, String> = Yaml().loadAll(File("$coinType.yml").readText()) as Map<String, String>
+
     }
 }

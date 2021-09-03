bdebfc22e635431236eab46245e75ac0973bc222
stupidfish <2928109164@qq.com>
2021-09-03 21:49:13 +0800

    邮箱注释

--- src/main/kotlin/com/stupidfish/tool/MailSender.kt
+++ src/main/kotlin/com/stupidfish/tool/MailSender.kt
@@ -5,6 +5,13 @@ import javax.mail.Session
 
 
 class MailSender(val mailName: String,val mailPassword: String,val mailUrl: String,val text: String, val oppositeMail: String) {
+    /*
+    mailName是发送者邮箱
+    mailPassword时发送者密码
+    mailUrl是smtp地址
+    text是发送信息
+    oppositeMail是目标邮箱
+     */
     fun send() {
         var prop = Properties()
         prop.setProperty("mail.host", "smtp.sohu.com")
